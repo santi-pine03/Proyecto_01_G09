002949f845849489e053f6a9a1d92d3272327b83
Sofia Losada <s.losadam@uniandes.edu.co>
2024-09-30 02:53:10 -0500

    CiudadControllerSofia

--- PROYECTO/src/main/java/uniandes/edu/co/proyecto/repositorio/CiudadRepository.java
+++ PROYECTO/src/main/java/uniandes/edu/co/proyecto/repositorio/CiudadRepository.java
@@ -1,11 +1,14 @@
 package uniandes.edu.co.proyecto.repositorio;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import jakarta.transaction.Transactional;
+import uniandes.edu.co.proyecto.modelo.Categoria;
 import uniandes.edu.co.proyecto.modelo.Ciudad;
 
 public interface CiudadRepository extends JpaRepository<Ciudad, Integer> {
@@ -15,4 +18,6 @@ public interface CiudadRepository extends JpaRepository<Ciudad, Integer> {
     void crearCiudad(@Param("codigo") Integer codigo,
                     @Param("nombre")String nombre);
     
+    @Query(value = "SELECT * FROM <Ciudades>", nativeQuery=true)
+    Collection<Ciudad>darCiudades();
 }
